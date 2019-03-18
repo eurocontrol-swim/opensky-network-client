745a332eaede43ece47dfd6d958d5f0174112d64
Alex Ntavelos <a.ntavelos@gmail.com>
2019-03-18 15:41:13 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Opensky Network API Client
---
+++ setup.py
@@ -0,0 +1,23 @@
+
+from setuptools import setup, find_packages
+
+__author__ = 'EUROCONTROL (SWIM)'
+
+setup(
+    name='opensky-network-client',
+    version='0.0.1',
+    description='Opensky Network Client',
+    author='EUROCONTROL (SWIM)',
+    author_email='',
+    packages=find_packages(exclude=['tests']),
+    url='https://bitbucket.org/antavelos-eurocontrol/opensky-network-client',
+    install_requires=[
+    ],
+    tests_require=[
+        'pytest',
+        'pytest-cov'
+    ],
+    platforms=['Any'],
+    license='see LICENSE',
+    zip_safe=False
+)
